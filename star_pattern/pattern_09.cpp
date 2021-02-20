db637c77f1eb0c18c6e8d3374930635c254bb6ff
Shivam Agrawal <43890874+s4shivam@users.noreply.github.com>
2021-02-20 21:43:57 +0530

    number pyramid

---
+++ star_pattern/pattern_09.cpp
@@ -0,0 +1,34 @@
+//     1
+//    121
+//   12321
+//  1234321
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        int k = 1;
+        for (int j = 1; j <= 7; j++)
+        {
+            if (j >= 5 - i && j <= 3 + i)
+            {
+                cout << k;
+                if (j < 4)
+                {
+                    k++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
