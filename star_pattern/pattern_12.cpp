909c1b7f85df1b9787e12b8c8521e96be70b06d5
Shivam Agrawal <43890874+s4shivam@users.noreply.github.com>
2021-02-21 23:54:49 +0530

    right pyramid

---
+++ star_pattern/pattern_12.cpp
@@ -0,0 +1,32 @@
+// *
+// **
+// ***
+// ****
+// ***
+// **
+// *
+#include <iostream>
+using namespace std;
+int main()
+{
+    int k = 0;
+    for (int i = 1; i <= 7; i++)
+    {
+        if (i <= 4)
+            k++;
+        else
+            k--;
+        for (int j = 1; j <= 4; j++)
+        {
+            if (j <= k)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
