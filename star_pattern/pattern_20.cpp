71b3ad2d332e64128c4325af9be6ee96b433913c
Shivam Agrawal <43890874+s4shivam@users.noreply.github.com>
2021-02-24 02:51:04 +0530

    alphabet pyramid

---
+++ star_pattern/pattern_20.cpp
@@ -0,0 +1,28 @@
+//    A
+//   ABA
+//  ABCBA
+// ABCDCBA
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char k;
+    for (int i = 1; i <= 4; i++)
+    {
+        k = 'A';
+        for (int j = 1; j <= 7; j++)
+        {
+            if (j >= 5 - i && j <= 3 + i)
+            {
+                cout << k;
+                j < 4 ? k++ : k--;
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
